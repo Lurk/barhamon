58547329a7e4eacaf41b3466662cb66d8ba64bda
Sergey Bargamon <sergey@bargamon.ru>
2020-07-01 18:08:36 +0300

    fix some typos

--- data/new_hope.tsx
+++ data/new_hope.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export const NewHopePreview: React.FC = () => {
   return (
     <>
-      <p>I want to write a blog as long as blogs exist. And now I'm doing it (hopefully).</p>
-      <p>Let start from the beginning.</p>
+      <p>I want to write a blog as long as blogs exist. Now I'm doing it (hopefully).</p>
+      <p>Let`s start from the beginning.</p>
       <p>My name Serhiy and I am a software developer.</p>
     </>
   )
--- data/this_blog.tsx
+++ data/this_blog.tsx
@@ -11,8 +11,8 @@ export const ThisBlogPreview: React.FC = () => {
         <List.Item><a href="https://date-fns.org/">date-fns</a></List.Item>
       </List>
       <p>Deployed on <a href="https://vercel.com/">Vercel</a>.</p>
-      <p>You can view source code on <a href="https://github.com/Lurk/barhamon">GitHub repo</a>. And if you find some
-        bug, or typo, or error, or you know ways to do things better - please submit <a
+      <p>You can find source code on <a href="https://github.com/Lurk/barhamon">GitHub repo</a>. If you find a bug, or
+        typo, or error, or you know ways to do things better - please submit <a
           href="https://github.com/Lurk/barhamon/issues">an issue</a>.</p>
     </>
   )
@@ -21,16 +21,7 @@ export const ThisBlogPreview: React.FC = () => {
 export const ThisBlogFull: React.FC = () => {
   return (
     <>
-      <p>This blog built with:</p>
-      <List as="ul">
-        <List.Item><a href="https://nextjs.org/">next.js</a></List.Item>
-        <List.Item><a href="https://react.semantic-ui.com/">Semantic UI React</a></List.Item>
-        <List.Item><a href="https://date-fns.org/">date-fns</a></List.Item>
-      </List>
-      <p>Deployed on <a href="https://vercel.com/">Vercel</a>.</p>
-      <p>You can view source code on <a href="https://github.com/Lurk/barhamon">GitHub repo</a>. And if you find some
-        bug, or typo, or error, or you know ways to do things better - please submit <a
-          href="https://github.com/Lurk/barhamon/issues">an issue</a>.</p>
+      <ThisBlogPreview/>
       <p>Roadmap:</p>
       <List ordered>
         <List.Item>Tags page</List.Item>
