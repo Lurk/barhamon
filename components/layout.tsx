dd0f1d76ecbc60296e0c637d8aecbd04889352da
Sergey Bargamon <sergey@bargamon.ru>
2020-07-13 14:40:50 +0300

    First attempt to do a footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,4 @@
-import { Container, Header } from "semantic-ui-react";
+import { Container, Grid, Header, Icon, Segment } from "semantic-ui-react";
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -22,7 +22,27 @@ export const Layout: React.FC<PageHeadProps> = (props) => {
       </main>
 
       <footer>
-
+        <Segment style={{ padding: '20px', marginTop: '20px' }} inverted>
+          <Container className="main">
+            <Grid inverted>
+              <Grid.Row columns={3}>
+                <Grid.Column>
+                  This is footer
+                </Grid.Column>
+                <Grid.Column>
+                  <p>You can find me at:</p>
+                  <p><a href="https://twitter.com/SergeyBargamon"><Icon className="twitter"/> Twitter</a></p>
+                  <p><a href="https://www.facebook.com/bargamon"><Icon className="facebook"/> Facebook</a></p>
+                  <p><a href="https://www.instagram.com/fedorpupkin/"><Icon className="instagram"/> instagramm</a></p>
+                </Grid.Column>
+                <Grid.Column>
+                  <p>Source code of this blog:</p>
+                  <p><a href="https://github.com/Lurk/barhamon"><Icon className="github"/> github</a></p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Segment>
       </footer>
     </>
   )
