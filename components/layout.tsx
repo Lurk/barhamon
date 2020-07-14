6eb0b83e946c5cd3707aa4e6f4adeaa96c2470c9
Sergey Bargamon <sergey@bargamon.ru>
2020-07-14 19:25:06 +0300

    fix footer layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -10,40 +10,55 @@ export const Layout: React.FC<PageHeadProps> = (props) => {
   return (
     <>
       <PageHead {...props}/>
-      <main>
-        <Container className="main">
-          <Header style={{ marginTop: "calc(2rem - .14285714em)" }} size="huge">
-            <Header.Content>{isIndex ? 'Serhiy Barhamon' :
-              <Link href="/"><a>Serhiy Barhamon</a></Link>}</Header.Content>
-            <Header.Subheader>software developer, motorcycle rider, fish catcher, nature lover</Header.Subheader>
-          </Header>
-          {props.children}
-        </Container>
-      </main>
+      <Container className="main">
+        <Header style={{ marginTop: "calc(2rem - .14285714em)" }} size="huge">
+          <Header.Content>{isIndex ? 'Serhiy Barhamon' :
+            <Link href="/"><a>Serhiy Barhamon</a></Link>}</Header.Content>
+          <Header.Subheader>software developer, motorcycle rider, fish catcher, nature lover</Header.Subheader>
+        </Header>
+        {props.children}
+      </Container>
 
-      <footer>
-        <Segment style={{ padding: '20px', marginTop: '20px' }} inverted>
-          <Container className="main">
-            <Grid inverted>
-              <Grid.Row columns={3}>
-                <Grid.Column>
-                  This is footer
-                </Grid.Column>
-                <Grid.Column>
-                  <p>You can find me at:</p>
-                  <p><a href="https://twitter.com/SergeyBargamon"><Icon className="twitter"/> Twitter</a></p>
-                  <p><a href="https://www.facebook.com/bargamon"><Icon className="facebook"/> Facebook</a></p>
-                  <p><a href="https://www.instagram.com/fedorpupkin/"><Icon className="instagram"/> instagramm</a></p>
-                </Grid.Column>
-                <Grid.Column>
-                  <p>Source code of this blog:</p>
-                  <p><a href="https://github.com/Lurk/barhamon"><Icon className="github"/> github</a></p>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Container>
-        </Segment>
-      </footer>
+      <Segment style={{ padding: '20px', marginTop: '20px', borderRadius: 0 }} inverted>
+        <Container className="main">
+          <Grid inverted>
+            <Grid.Row columns={3} only="computer tablet">
+              <Grid.Column>
+                This is footer
+              </Grid.Column>
+              <Grid.Column>
+                <p>You can find me at:</p>
+                <p><a href="https://twitter.com/SergeyBargamon"><Icon className="twitter"/> Twitter</a></p>
+                <p><a href="https://www.facebook.com/bargamon"><Icon className="facebook"/> Facebook</a></p>
+                <p><a href="https://www.instagram.com/fedorpupkin/"><Icon className="instagram"/> instagramm</a></p>
+              </Grid.Column>
+              <Grid.Column>
+                <p>Source code of this blog:</p>
+                <p><a href="https://github.com/Lurk/barhamon"><Icon className="github"/> github</a></p>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={1} only="mobile">
+              <Grid.Column>
+                This is footer
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={1} only="mobile">
+              <Grid.Column>
+                <p>You can find me at:</p>
+                <p><a href="https://twitter.com/SergeyBargamon"><Icon className="twitter"/> Twitter</a></p>
+                <p><a href="https://www.facebook.com/bargamon"><Icon className="facebook"/> Facebook</a></p>
+                <p><a href="https://www.instagram.com/fedorpupkin/"><Icon className="instagram"/> instagramm</a></p>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={1} only="mobile">
+              <Grid.Column>
+                <p>Source code of this blog:</p>
+                <p><a href="https://github.com/Lurk/barhamon"><Icon className="github"/> github</a></p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Segment>
     </>
   )
 }
