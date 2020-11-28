cd2f7b8e4bf57350b371ee210ee0e542b097a4f8
Sergey Bargamon <sergey@bargamon.ru>
2020-11-28 09:48:40 +0200

    fix word choise

--- data/comments.tsx
+++ data/comments.tsx
@@ -6,9 +6,9 @@ const code = `import { DiscussionEmbed } from "disqus-react"
 <DiscussionEmbed
   shortname={"remember that short name from second step?"}
   config={{
-    url: "url for this page",
+    url: "this page url",
     identifier: "unique identifier of this page",
-    title: "title for this page"
+    title: "this page title"
   }}
 />
 `;
@@ -16,7 +16,7 @@ const code = `import { DiscussionEmbed } from "disqus-react"
 
 const CommentsPreview: React.FC = () => {
   return (
-      <p>In this post, you will learn how to add comments to your Next.js project. And it is easy with Disqus. </p>
+    <p>In this post, you will learn how to add comments to your Next.js project. And it is easy with Disqus. </p>
   )
 }
 
@@ -25,16 +25,18 @@ const CommentsFull: React.FC = () => {
     <>
       <CommentsPreview/>
       <p>First, create an account on <a href="https://disqus.com/">Disqus</a> .</p>
-
-      <p>Second, go to the <a href="https://disqus.com/profile/signup/intent/">https://disqus.com/profile/signup/intent/</a> and press "I want to install Disqus on my site", fill-up the form, and remember the short name (it will be useful later).</p>
-
-      <p>Third, install the <a href="https://www.npmjs.com/package/disqus-react">disqus-react</a> package from npm.  If you are a Typescript user, this package already has typings.</p>
+      <p>Second, go to the <a
+        href="https://disqus.com/profile/signup/intent/">https://disqus.com/profile/signup/intent/</a> and press "I want
+        to install Disqus on my site", fill-up the form, and remember the short name (it will be useful later).</p>
+      <p>Third, install the <a href="https://www.npmjs.com/package/disqus-react">disqus-react</a> package from npm. If
+        you are a Typescript user, this package already has typings.</p>
       <Code language={"bash"}>{`npm install disqus-react`}</Code>
       <p>And use it like this:</p>
       <Code language="jsx">
         {code}
       </Code>
-      <p>You can find a usage example here: <a href="https://github.com/Lurk/barhamon/blob/master/components/comments.tsx">comments.tsx</a> </p>
+      <p>You can find a usage example here: <a
+        href="https://github.com/Lurk/barhamon/blob/master/components/comments.tsx">comments.tsx</a></p>
     </>
   )
 }
