95617db53734dbe8a9b33b65091ddafea9c960a9
Sergey Bargamon <sergey@bargamon.ru>
2020-12-02 22:52:02 +0200

    Fix image sizes in post preview

--- components/post_preview.tsx
+++ components/post_preview.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { Card, Image } from "semantic-ui-react";
+import { Card } from "semantic-ui-react";
 import Link from "next/link";
 import type { PostInterface } from "../models/posts";
 import { PostMeta } from "./post_meta";
 import styles from './post_preview.module.css'
+import { useRouter } from "next/router";
 
 export const PostPreview: React.FC<PostInterface> = ({ Preview, Full, ...rest }) => {
-
+  const router = useRouter();
   return (
     <Card>
       {rest.image &&
-      <Link as={`/post/${rest.pid}`} href={`/post/[pid]`}>
-        <Image src={rest.image} wrapped ui={false} className={styles.header_image} href={`/post/${rest.pid}`}/>
-      </Link>
+        <div
+          style={{ backgroundImage: `url(${rest.image})`}}
+          className={styles.header_image}
+          onClick={()=>router.push(`/post/[pid]`, `/post/${rest.pid}`)}
+        />
       }
       <Card.Content>
         <Card.Header>
