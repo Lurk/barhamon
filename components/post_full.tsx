4455878efd0e9b5fa91401d53a845ffbd0764eaa
Sergey Bargamon <sergey@bargamon.ru>
2020-12-04 23:00:59 +0200

    add more color to layout

--- components/post_full.tsx
+++ components/post_full.tsx
@@ -11,28 +11,28 @@ export const PostFull: React.FC<PostInterface> = ({ Full, ...rest }) => {
     <Grid>
       <Grid.Row>
         <Grid.Column computer={10} mobile={16}>
-      <Item>
-        <Item.Content className={styles.content}>
-          <div style={{ backgroundImage: `url(${rest.image})` }} className={styles.header_container}>
-            <div className={styles.header_text}>
-              <Header inverted size="large">
-                <Header.Content>{rest.header}</Header.Content>
-                <Header.Subheader>{getPostMeta(rest.time, rest.readTime)}</Header.Subheader>
-              </Header>
-            </div>
-            <div className={styles.header_tags}>
-              {rest.tags.map((tag, id) => <Tag tag={tag} id={id} key={id} className={styles.tag}/>)}
-            </div>
-          </div>
-          <Item.Description>
-            <Full/>
-          </Item.Description>
-          <Divider/>
-          <Item.Extra>
-            <Comments slug={rest.pid} id={rest.pid} title={rest.header}/>
-          </Item.Extra>
-        </Item.Content>
-      </Item>
+          <Item>
+            <Item.Content className={styles.content}>
+              <div style={{ backgroundImage: `url(${rest.image})` }} className={styles.header_container}>
+                <div className={styles.header_text}>
+                  <Header inverted size="large">
+                    <Header.Content>{rest.header}</Header.Content>
+                    <Header.Subheader>{getPostMeta(rest.time, rest.readTime)}</Header.Subheader>
+                  </Header>
+                </div>
+                <div className={styles.header_tags}>
+                  {rest.tags.map((tag, id) => <Tag tag={tag} id={id} key={id} className={styles.tag}/>)}
+                </div>
+              </div>
+              <Item.Description className={styles.pad}>
+                <Full/>
+              </Item.Description>
+              <Divider/>
+              <Item.Extra className={styles.pad}>
+                <Comments slug={rest.pid} id={rest.pid} title={rest.header}/>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
         </Grid.Column>
       </Grid.Row>
     </Grid>
