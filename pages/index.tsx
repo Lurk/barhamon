ebfbde65cddff3b700116a3141862f5e0c654151
Sergey Bargamon <sergey@bargamon.ru>
2020-12-30 20:24:00 +0200

    getInitialProps => getStaticProps to allow nextJS prerender page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { posts } from "../data";
 import { Layout } from "../components/layout";
 import { PostList } from "../components/post_list";
+import { GetStaticProps } from "next";
+import { PageResult } from "../models/posts";
+import { Params } from "next/dist/next-server/server/router";
 
 const limit = parseInt(process.env.NEXT_PUBLIC_POSTS_PER_PAGE);
 
@@ -13,6 +16,10 @@ export default function Home({ page }) {
   )
 }
 
-Home.getInitialProps = async () => {
-  return { page: posts.getPage({ limit, offset: 0 }) }
+export const getStaticProps: GetStaticProps<{ page: PageResult }, Params> = async () => {
+  return {
+    props: {
+      page: posts.getPage({ limit, offset: 0})
+    }
+  }
 }
