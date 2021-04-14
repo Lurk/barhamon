87792de918d6fa846577aa9c223b97e9a66bde29
Sergey Bargamon <sergey@bargamon.ru>
2021-04-14 22:13:31 +0200

    fix feed.xml generation

--- pages/feed.xml.tsx
+++ pages/feed.xml.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import formatRFC7231 from 'date-fns/formatRFC3339'
+import formatRFC7231 from "date-fns/formatRFC3339";
 import { posts } from "../data";
 import type { PostInterface } from "../models/posts";
 import { memoize } from "../utils";
@@ -7,16 +7,33 @@ import { memoize } from "../utils";
 const POST_URL_PREFIX = `${process.env.NEXT_PUBLIC_HOST}/post/`;
 
 const createJsonFeed = memoize((posts: PostInterface[]) => {
-  return `<rss version="2.0"><channel><title>${process.env.NEXT_PUBLIC_TITLE}</title><link>${process.env.NEXT_PUBLIC_HOST}</link><language>en-us</language><image><url>${process.env.NEXT_PUBLIC_HOST}/mstile-150x150.png</url><title>${process.env.NEXT_PUBLIC_TITLE}</title><link>${process.env.NEXT_PUBLIC_HOST}</link></image><description>${process.env.NEXT_PUBLIC_TITLE} blog</description>${posts.map(renderPost).join("")}</channel></rss>`;
+  return `<rss version="2.0"><channel><title>${
+    process.env.NEXT_PUBLIC_TITLE
+  }</title><link>${
+    process.env.NEXT_PUBLIC_HOST
+  }</link><language>en-us</language><image><url>${
+    process.env.NEXT_PUBLIC_HOST
+  }/mstile-150x150.png</url><title>${
+    process.env.NEXT_PUBLIC_TITLE
+  }</title><link>${process.env.NEXT_PUBLIC_HOST}</link></image><description>${
+    process.env.NEXT_PUBLIC_TITLE
+  } blog</description>${posts.map(renderPost).join("")}</channel></rss>`;
 });
 
 function renderPost(post: PostInterface) {
-  return `<item><title>${post.header}</title><link>${POST_URL_PREFIX}${post.pid}</link><description>${post.description}</description><pubDate>${formatRFC7231(post.time)}</pubDate><guid guid isPermaLink="true">${POST_URL_PREFIX}${post.pid}</guid></item>`;
+  return `<item><title>${post.header}</title><link>${POST_URL_PREFIX}${
+    post.pid
+  }</link><description>${
+    post.description
+  }</description><pubDate>${formatRFC7231(
+    post.time
+  )}</pubDate><guid isPermaLink="true">${POST_URL_PREFIX}${
+    post.pid
+  }</guid></item>`;
 }
 
-
 export async function getServerSideProps({ res }) {
-  res.setHeader('Content-Type', 'application/rss+xml');
+  res.setHeader("Content-Type", "application/rss+xml");
   res.write(createJsonFeed(posts.getAll()));
   res.end();
 
@@ -26,6 +43,6 @@ export async function getServerSideProps({ res }) {
   return { props: {} };
 }
 
-const Sitemap: React.FC = () => (<></>)
+const Sitemap: React.FC = () => <></>;
 
 export default Sitemap;
