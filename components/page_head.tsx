354104609a164cc1f9096580d38a7c5823b6c6f4
serhiy.barhamon <serhiy.barhamon@check24.de>
2021-09-11 21:42:25 +0200

    revert ga changes

--- components/page_head.tsx
+++ components/page_head.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Head from "next/head";
-import Script from "next/script";
 
 export interface PageHeadProps {
   title?: string;
@@ -44,80 +43,82 @@ export const PageHead: React.FC<PageHeadProps> = ({
   const imgUrl = getImageUrl(image);
 
   return (
-    <>
-      <Head>
-        <title>{t}</title>
-        <meta property="og:title" content={t} />
-        <meta property="og:type" content="website" />
-        {canonical && (
-          <meta
-            property="og:url"
-            content={`${process.env.NEXT_PUBLIC_HOST}${canonical}`}
-          />
-        )}
-        <meta property="og:image" content={imgUrl} />
+    <Head>
+      <title>{t}</title>
+      <meta property="og:title" content={t} />
+      <meta property="og:type" content="website" />
+      {canonical && (
         <meta
-          property="og:description"
-          content={description || "This is the Blog :)"}
+          property="og:url"
+          content={`${process.env.NEXT_PUBLIC_HOST}${canonical}`}
         />
-        {canonical && (
-          <link
-            rel="canonical"
-            href={`${process.env.NEXT_PUBLIC_HOST}${canonical}`}
-          />
-        )}
+      )}
+      <meta property="og:image" content={imgUrl} />
+      <meta
+        property="og:description"
+        content={description || "This is the Blog :)"}
+      />
+      {canonical && (
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
+          rel="canonical"
+          href={`${process.env.NEXT_PUBLIC_HOST}${canonical}`}
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#8e8e8e" />
-        <link
-          rel="alternate"
-          type="application/feed+json"
-          title="JSON Feed"
-          href={`${process.env.NEXT_PUBLIC_HOST}/feed.json`}
-        />
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href={`${process.env.NEXT_PUBLIC_HOST}/feed.xml`}
-        />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="author" content="Serhiy Barhamon" />
-        <meta name="charset" content="UTF-8" />
-        <meta
-          name="description"
-          content={description ? description : "Serhiy Barhamon personal blog"}
-        />
-        <meta name="keywords" content={u.join(", ")} />
-      </Head>
-      <Script
-        strategy="lazyOnload"
+      )}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#8e8e8e" />
+      <link
+        rel="alternate"
+        type="application/feed+json"
+        title="JSON Feed"
+        href={`${process.env.NEXT_PUBLIC_HOST}/feed.json`}
+      />
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        title="RSS"
+        href={`${process.env.NEXT_PUBLIC_HOST}/feed.xml`}
+      />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="author" content="Serhiy Barhamon" />
+      <meta name="charset" content="UTF-8" />
+      <meta
+        name="description"
+        content={description ? description : "Serhiy Barhamon personal blog"}
+      />
+      <meta name="keywords" content={u.join(", ")} />
+
+      <script
+        async
         src="https://www.googletagmanager.com/gtag/js?id=UA-171846183-1"
       />
-      <Script strategy="lazyOnload" id="ga">
-        {`
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'UA-171846183-1');`}
-      </Script>
-    </>
+
+  gtag('config', 'UA-171846183-1');`,
+        }}
+      />
+    </Head>
   );
 };
